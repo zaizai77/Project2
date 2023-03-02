eb8127a74d191dd44d8a239005f88063c2f00e58
zaizai <2295985992@qq.com>
2023-03-02 11:11:25 +0800

    test

--- 源.cpp
+++ 源.cpp
@@ -7,6 +7,5 @@ int tadd_ok(int x, int y)
 {
     int sum = x + y;
     int neg_over = x < 0 && y < 0 && sum>0;
-    int ops_over = x > 0 && y > 0 && sum < 0;
-    return !neg_over && !ops_over;
+    return !neg_over;
 }
